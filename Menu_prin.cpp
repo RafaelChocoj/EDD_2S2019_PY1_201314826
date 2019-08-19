5493c8f06d2c2e3c248fde64054c5ee5baff7ef1
RafaelChocoj <rafaelcliveguitar@gmail.com>
2019-08-19 16:12:00 -0600

    primer commit de semana 1

---
+++ Menu_prin.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout <<"***********MENU***********\n\n";
+	cout <<"1. Insert Image"<<endl;
+	cout <<"2. Select Image"<<endl;
+    bool men = true;
+    while(men)
+    {
+        
+
+
+    }
+}
